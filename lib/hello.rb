9ee4a1df192c425f60cf00e3230cee3c37986f2c
Srikalyan Megati <kalmegati@gmail.com>
2019-09-14 03:47:54 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,19 @@
 def hello_t(array)
-#  array.length.times { |index| yield array[index] }
-  i = 0
+  if block_given?
+    i = 0
  
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
   
-  array
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
+
+# array.length.times { |index| yield array[index] }
 
 # call your method here!
 
