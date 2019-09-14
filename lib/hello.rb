1c6254db968bb11fa085988583a8f94fefc33ba3
Srikalyan Megati <kalmegati@gmail.com>
2019-09-14 03:44:04 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,21 @@
-def hello_t
-
+def hello_t(array)
+#  array.length.times { |index| yield array[index] }
+  i = 0
+ 
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  
+  array
 end
 
 # call your method here!
 
+cattos = ["Tim", "Tom", "Jim"]
+
+hello_t(cattos) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
